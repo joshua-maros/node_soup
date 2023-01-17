f67d25e1720dea8023835db3bdcd611e6ff416e2
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2023-01-16 16:44:18 -0800

    I think this is the way to go

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,11 +19,12 @@ class MyApp extends StatelessWidget {
       ),
       darkTheme: ThemeData.from(
         colorScheme: ColorScheme.fromSwatch(
-            brightness: Brightness.dark,
-            accentColor: Colors.orange,
-            backgroundColor: const Color(0xFF101010),
-            primaryColorDark: Colors.orange,
-            cardColor: const Color(0xFF101010)),
+          brightness: Brightness.dark,
+          accentColor: Colors.orange,
+          backgroundColor: const Color(0xFF000000),
+          primaryColorDark: Colors.orange,
+          cardColor: const Color(0xFF101010),
+        ),
       ),
       themeMode: ThemeMode.dark,
       home: const MyHomePage(title: 'Flutter Demo Home Page 5'),
@@ -29,123 +32,138 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Node extends StatelessWidget {
-  final List<Key?> socketKeys;
-  const Node(this.socketKeys, {super.key});
+const nodeWidth = 128.0;
+const nodeHeaderHeight = 32.0;
+const nodeHeaderFontSize = 16.0;
+const nodeCornerRadius = Radius.circular(16);
+const nodeSocketHeight = 32.0;
+const nodeSocketPadding = 8.0;
+const nodeBackgroundColor = Color(0xFF101010);
 
-  @override
-  Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: NodePainter(context: context),
-      child: ConstrainedBox(
-        constraints: const BoxConstraints.tightFor(width: 128),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const NodeHeader('ADD'),
-            NodeOutput(
-              'Output 1',
-              key: socketKeys[0],
-            ),
-            NodeOutput(
-              'Output 2',
-              key: socketKeys[1],
-            ),
-            NodeInput(
-              'Input 1',
-              key: socketKeys[2],
-            ),
-            NodeInput(
-              'Input 2',
-              key: socketKeys[3],
-            ),
-            const SizedBox(height: 8),
-          ],
-        ),
-      ),
-    );
-  }
+class VisualWire {
+  final VisualNode start, end;
+  final int startIndex, endIndex;
+
+  const VisualWire(this.start, this.startIndex, this.end, this.endIndex);
 }
 
-class NodeHeader extends StatelessWidget {
+class VisualSocket {
   final String label;
+  final Color color;
 
-  const NodeHeader(this.label, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: CustomPaint(
-        painter: NodeHeaderPainter(),
-        child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: Text(label,
-              textAlign: TextAlign.center,
-              style: const TextStyle(fontWeight: FontWeight.bold)),
-        ),
-      ),
-    );
-  }
+  const VisualSocket(this.label, {this.color = Colors.blue});
 }
 
-class NodeInput extends StatelessWidget {
+class VisualNode {
   final String label;
+  final Color color;
+  final List<VisualSocket> inputSockets, outputSockets;
+  final Offset position;
 
-  const NodeInput(this.label, {super.key});
+  const VisualNode(
+    this.position,
+    this.label,
+    this.inputSockets,
+    this.outputSockets, {
+    this.color = Colors.red,
+  });
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 8, 16, 0),
-        child: CustomPaint(
-          painter: NodeInputPainter(),
-          child: Padding(
-            padding: const EdgeInsets.all(4),
-            child: Text(label, textAlign: TextAlign.left),
-          ),
-        ),
-      ),
-    );
+  Offset outputSocketConnectionPoint(int index) {
+    return position +
+        Offset(
+          nodeWidth,
+          nodeHeaderHeight +
+              nodeSocketPadding +
+              index * (nodeSocketPadding + nodeSocketHeight) +
+              nodeSocketHeight / 2,
+        );
+  }
+
+  Offset inputSocketConnectionPoint(int index) {
+    return position +
+        Offset(
+          0,
+          nodeHeaderHeight +
+              nodeSocketPadding +
+              (outputSockets.length + index) *
+                  (nodeSocketPadding + nodeSocketHeight) +
+              nodeSocketHeight / 2,
+        );
+  }
+
+  double height() {
+    return nodeHeaderHeight +
+        (inputSockets.length + outputSockets.length) *
+            (nodeSocketPadding + nodeSocketHeight) +
+        max(nodeCornerRadius.y, nodeSocketPadding);
   }
 }
 
-class NodeOutput extends StatelessWidget {
-  final String label;
-
-  const NodeOutput(this.label, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 8, 0, 0),
-        child: CustomPaint(
-          painter: NodeOutputPainter(),
-          child: Padding(
-            padding: const EdgeInsets.all(4),
-            child: Text(label, textAlign: TextAlign.right),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class NodePainter extends CustomPainter {
+class NodeGraphPainter extends CustomPainter {
   BuildContext context;
+  List<VisualNode> nodes;
+  List<VisualWire> wires;
 
-  NodePainter({required this.context});
+  NodeGraphPainter(this.context, this.nodes, this.wires);
 
   @override
   void paint(Canvas canvas, Size size) {
+    for (var wire in wires) {
+      paintWire(canvas, wire);
+    }
+    for (var node in nodes) {
+      paintNode(canvas, node);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant NodeGraphPainter oldDelegate) {
+    return context != oldDelegate.context;
+  }
+
+  void paintWire(Canvas canvas, VisualWire wire) {
+    final paint = Paint();
+    paint.color = wire.start.outputSockets[wire.startIndex].color;
+    final start = wire.start.outputSocketConnectionPoint(wire.startIndex);
+    final end = wire.end.inputSocketConnectionPoint(wire.endIndex);
+    canvas.drawCircle(
+      start,
+      nodeSocketHeight / 2,
+      paint,
+    );
+    canvas.drawCircle(
+      end,
+      nodeSocketHeight / 2,
+      paint,
+    );
+    paint.strokeWidth = nodeSocketHeight / 4;
+    canvas.drawLine(start, end, paint);
+  }
+
+  void paintNode(Canvas canvas, VisualNode node) {
+    canvas.save();
+    canvas.translate(node.position.dx, node.position.dy);
+    paintNodeBackground(
+      canvas,
+      node.height(),
+    );
+    paintNodeHeader(canvas, node);
+    canvas.translate(0, nodeHeaderHeight + nodeSocketPadding);
+    for (var socket in node.outputSockets) {
+      paintNodeOutput(canvas, socket);
+      canvas.translate(0, nodeSocketHeight + nodeSocketPadding);
+    }
+    for (var socket in node.inputSockets) {
+      paintNodeInput(canvas, socket);
+      canvas.translate(0, nodeSocketHeight + nodeSocketPadding);
+    }
+    canvas.restore();
+  }
+
+  void paintNodeBackground(Canvas canvas, double height) {
     var paint = Paint();
-    paint.color = Theme.of(context).colorScheme.background;
-    final r = RRect.fromLTRBR(
-        0, 0, size.width, size.height, const Radius.circular(8));
+    paint.color = nodeBackgroundColor;
+    final r = RRect.fromLTRBR(0, 0, nodeWidth, height, nodeCornerRadius);
     canvas.drawRRect(r, paint);
     paint.style = PaintingStyle.stroke;
     paint.color = Colors.grey.shade700;
@@ -153,99 +171,65 @@ class NodePainter extends CustomPainter {
     canvas.drawRRect(r, paint);
   }
 
-  @override
-  bool shouldRepaint(covariant NodePainter oldDelegate) {
-    return context != oldDelegate.context;
+  void drawText(Canvas canvas, String text, Rect boundingBox,
+      {FontWeight? weight, Alignment alignment = Alignment.center}) {
+    final textStyle =
+        TextStyle(fontSize: nodeHeaderFontSize, fontWeight: weight);
+    final textSpan = TextSpan(text: text, style: textStyle);
+    final textPainter =
+        TextPainter(text: textSpan, textDirection: TextDirection.ltr);
+    textPainter.layout(minWidth: 0, maxWidth: nodeWidth);
+    final dx = boundingBox.size.width - textPainter.width;
+    final dy = boundingBox.size.height - textPainter.height;
+    final alignBox =
+        Rect.fromLTWH(boundingBox.topLeft.dx, boundingBox.topLeft.dy, dx, dy);
+    textPainter.paint(canvas, alignment.withinRect(alignBox));
   }
-}
 
-class NodeHeaderPainter extends CustomPainter {
-  NodeHeaderPainter();
-
-  @override
-  void paint(Canvas canvas, Size size) {
+  void paintNodeHeader(Canvas canvas, VisualNode node) {
     var paint = Paint();
-    paint.color = Colors.red;
-    var z = const Radius.circular(0);
-    var e = const Radius.circular(8);
-    final r = RRect.fromLTRBAndCorners(0, 0, size.width, size.height,
-        bottomLeft: z, bottomRight: z, topLeft: e, topRight: e);
+    paint.color = node.color;
+    final r = RRect.fromLTRBAndCorners(0, 0, nodeWidth, nodeHeaderHeight,
+        topLeft: nodeCornerRadius, topRight: nodeCornerRadius);
     canvas.drawRRect(r, paint);
+    drawText(
+      canvas,
+      node.label,
+      const Rect.fromLTWH(0, 0, nodeWidth, nodeHeaderHeight),
+      weight: FontWeight.bold,
+    );
   }
 
-  @override
-  bool shouldRepaint(covariant NodeHeaderPainter oldDelegate) {
-    return false;
-  }
-}
-
-class NodeInputPainter extends CustomPainter {
-  NodeInputPainter();
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint();
-    paint.color = Colors.blue;
-    const z = Radius.circular(0);
-    const e = Radius.circular(8);
-    final r = RRect.fromLTRBAndCorners(0, 0, size.width, size.height,
-        bottomLeft: z, bottomRight: e, topLeft: z, topRight: e);
-    canvas.drawRRect(r, paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant NodeInputPainter oldDelegate) {
-    return false;
-  }
-}
-
-class NodeOutputPainter extends CustomPainter {
-  NodeOutputPainter();
-
-  @override
-  void paint(Canvas canvas, Size size) {
+  void paintNodeOutput(Canvas canvas, VisualSocket socket) {
     var paint = Paint();
-    paint.color = Colors.blue;
-    var z = const Radius.circular(0);
-    var e = const Radius.circular(8);
-    final r = RRect.fromLTRBAndCorners(0, 0, size.width, size.height,
-        bottomLeft: e, bottomRight: z, topLeft: e, topRight: z);
+    paint.color = socket.color;
+    final r = RRect.fromLTRBAndCorners(
+        2 * nodeSocketPadding, 0, nodeWidth, nodeSocketHeight,
+        topLeft: nodeCornerRadius, bottomLeft: nodeCornerRadius);
     canvas.drawRRect(r, paint);
+    drawText(
+      canvas,
+      socket.label,
+      const Rect.fromLTRB(
+          3 * nodeSocketPadding, 0, nodeWidth, nodeSocketHeight),
+      alignment: Alignment.centerLeft,
+    );
   }
 
-  @override
-  bool shouldRepaint(covariant NodeOutputPainter oldDelegate) {
-    return false;
-  }
-}
-
-class NodeWirePainter extends CustomPainter {
-  final GlobalKey socket0, socket1, node2;
-  final GlobalKey self;
-
-  const NodeWirePainter(this.self, this.socket0, this.socket1, this.node2);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final sp = (self.currentContext?.findRenderObject() as RenderBox)
-        .localToGlobal(Offset.zero);
-    const padding = 8.0;
-    const paddingOffset = Offset(0, padding);
-    final s0 = socket0.currentContext?.findRenderObject() as RenderBox;
-    final s0p = s0.size.centerRight(s0.localToGlobal(Offset.zero)) + paddingOffset / 2;
-    final s1 = socket1.currentContext?.findRenderObject() as RenderBox;
-    final s1p = s1.size.centerLeft(s1.localToGlobal(Offset.zero)) + paddingOffset / 2;
+  void paintNodeInput(Canvas canvas, VisualSocket socket) {
     var paint = Paint();
-    paint.color = Colors.blue;
-    paint.strokeWidth = 8;
-    canvas.drawLine(s0p - sp, s1p - sp, paint);
-    canvas.drawCircle(s0p - sp, (s0.size.height - padding) / 2, paint);
-    canvas.drawCircle(s1p - sp, (s1.size.height - padding) / 2, paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
+    paint.color = socket.color;
+    final r = RRect.fromLTRBAndCorners(
+        0, 0, nodeWidth - 2 * nodeSocketPadding, nodeSocketHeight,
+        topRight: nodeCornerRadius, bottomRight: nodeCornerRadius);
+    canvas.drawRRect(r, paint);
+    drawText(
+      canvas,
+      socket.label,
+      const Rect.fromLTRB(nodeSocketPadding, 0,
+          nodeWidth - 3 * nodeSocketPadding, nodeSocketHeight),
+      alignment: Alignment.centerLeft,
+    );
   }
 }
 
@@ -259,17 +243,29 @@ class NodeGraph extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const nodes = [
+      VisualNode(
+        Offset(0, 0),
+        'ADD',
+        [VisualSocket('a'), VisualSocket('b')],
+        [VisualSocket('result')],
+      ),
+      VisualNode(
+        Offset(2 * nodeWidth, 0),
+        'ADD',
+        [VisualSocket('a'), VisualSocket('b')],
+        [VisualSocket('result')],
+      ),
+    ];
+    final wires = [
+      VisualWire(nodes[0], 0, nodes[1], 0),
+      VisualWire(nodes[0], 0, nodes[1], 1),
+    ];
     return Stack(
       children: [
         CustomPaint(
           key: graph,
-          painter: NodeWirePainter(graph, socket0, socket1, socket2),
-        ),
-        Node([null, socket0, null, null]),
-        Positioned(
-          left: 200,
-          top: 100,
-          child: Node([null, null, socket1, null]),
+          painter: NodeGraphPainter(context, nodes, wires),
         ),
       ],
     );
@@ -301,7 +297,10 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: SizedBox(
-          width: double.infinity, height: double.infinity, child: NodeGraph()),
+        width: double.infinity,
+        height: double.infinity,
+        child: NodeGraph(),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
